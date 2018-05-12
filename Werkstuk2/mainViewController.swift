76f5925505ca5fead9acde1b75e464253609fcfc
Ketrilawy <kelly.tordeur@student.ehb.be>
2018-05-12 10:45:16 +0200

    start

---
+++ Werkstuk2/mainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  mainViewController.swift
+//  Werkstuk2
+//
+//  Created by student on 12/05/18.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class mainViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet var button: UIButton!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
